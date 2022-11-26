83732d3aad879cfc62cb3eb69389ba5c49892b01
developer-mahin <mdmahin1310@gmail.com>
2022-11-26 13:16:10 +0600

    create a add to wishlist handler and add product in the wishlist

--- src/Pages/Home/CategoriesSection/ProductOfCategory/SingleProductCard.js
+++ src/Pages/Home/CategoriesSection/ProductOfCategory/SingleProductCard.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import toast from 'react-hot-toast';
+import PrimarySpinner from '../../../../components/Spinner/PrimarySpinner';
 
 const SingleProductCard = ({ product, setModalData }) => {
     const { location, name, original_price, published_date, picture, rating, resale_price, seller_img, verify, seller_name, years_of_use } = product;
+    const [loading, setLoading] = useState(false)
+
+    const handleAddToWishList = (product) => {
+        setLoading(true)
+        fetch("http://localhost:5000/wishlist", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+                authorization: `bearer ${localStorage.getItem("access-token")}`
+            },
+            body: JSON.stringify(product)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged) {
+                    toast.success("Successfully added in the wishlist");
+                    setLoading(false)
+                }
+            })
+            .catch(err => {
+                toast.error(err.message)
+                setLoading(false)
+            })
+    }
 
     return (
         <li className="flex flex-col py-6 sm:flex-row sm:justify-between">
@@ -60,11 +86,17 @@ const SingleProductCard = ({ product, setModalData }) => {
                             </svg>
                             <span>Book Now</span>
                         </label>
-                        <button type="button" className="flex items-center px-2 py-1 space-x-1">
+                        <button
+                            onClick={() => handleAddToWishList(product)}
+                            type="button"
+                            className="flex items-center px-2 py-1 space-x-1"
+                        >
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 h-4 fill-current">
                                 <path d="M453.122,79.012a128,128,0,0,0-181.087.068l-15.511,15.7L241.142,79.114l-.1-.1a128,128,0,0,0-181.02,0l-6.91,6.91a128,128,0,0,0,0,181.019L235.485,449.314l20.595,21.578.491-.492.533.533L276.4,450.574,460.032,266.94a128.147,128.147,0,0,0,0-181.019ZM437.4,244.313,256.571,425.146,75.738,244.313a96,96,0,0,1,0-135.764l6.911-6.91a96,96,0,0,1,135.713-.051l38.093,38.787,38.274-38.736a96,96,0,0,1,135.765,0l6.91,6.909A96.11,96.11,0,0,1,437.4,244.313Z"></path>
                             </svg>
-                            <span>Add to Wishlist</span>
+                            {
+                                loading ? <PrimarySpinner></PrimarySpinner> : "Add to Wishlist"
+                            }
                         </button>
                     </div>
                 </div>
